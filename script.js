21f6180042d2e18a93097f180368157b6864bf8e
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-15 23:30:05 +0200

    finito mancante parte stile

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const cardContainer = document.querySelector(".card-container");
 const newMemberName = document.getElementById("new-member-name");
 const newMemberRole = document.getElementById("new-member-role");
-const newMemberEmail = document.getElementById("new-member-Email");
+const newMemberEmail = document.getElementById("new-member-email");
+const newMemberImg = document.getElementById("new-member-img");
 const addNewMember = document.getElementById("add-new-member");
-
+const newMemberButton = document.getElementById("new-member-button");
+const formContainer = document.getElementById("form-container");
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -45,13 +47,14 @@ const teamMembers = [
 
 let cardsHTML = ``;
 for (const member of teamMembers) {
+  const { name, role, email, img } = member;
   cardsHTML += `<div class ="col-4">
         <div class = "card d-flex">
-          <div class ="card-img"><img src="${member.img}" alt="${member.name}" /></div>
+          <div class ="card-img"><img src="${img}" alt="${name}" /></div>
           <div class ="card-info bg-dark">
-            <h3 class ="fw-bold text-white">${member.name}</h3>
-            <p class ="text-white fs-4">${member.role}</p>
-            <p class ="text-primary fs-4">${member.email}</p>
+            <h3 class ="fw-bold text-white">${name}</h3>
+            <p class ="text-white fs-4">${role}</p>
+            <p class ="text-primary fs-4">${email}</p>
           </div>
         </div>
       </div>`;
@@ -63,7 +66,23 @@ addNewMember.addEventListener("submit", (e) => {
   const newName = newMemberName.value;
   const newRole = newMemberRole.value;
   const newEmail = newMemberEmail.value;
+  const newImg = newMemberImg.value;
 
-  const newMember = (newName, newRole, newEmail);
+  const newMember = { newName, newRole, newEmail, newImg };
   teamMembers.push(newMember);
+
+  let newCardsHTML = `<div class ="col-4">
+        <div class = "card d-flex">
+          <div class ="card-img"><img src="${newImg}" alt="${newName}" /></div>
+          <div class ="card-info bg-dark">
+            <h3 class ="fw-bold text-white">${newName}</h3>
+            <p class ="text-white fs-4">${newRole}</p>
+            <p class ="text-primary fs-4">${newEmail}</p>
+          </div>
+        </div>
+      </div>`;
+
+  formContainer.innerHTML += newCardsHTML;
+  console.log(newMember);
 });
+console.log(teamMembers);
